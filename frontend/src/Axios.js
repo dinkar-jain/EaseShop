9ff1858ef354020757f058fedc42aca85fc38fb3
Dinkar <jaindinkar@ymail.com>
2024-08-18 15:03:00 +0530

    chore: Added vercel.json

--- frontend/src/Axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = "http://localhost:5000";
-
-const instance = axios.create({
-    baseURL: API_URL,
-});
-
-export default instance;
--- frontend/src/redux/reducers/authReducer.js
+++
@@ -1,9 +0,0 @@
-export const signInReducer = (state = {}, action) => {
-    if (action.type === "SIGN_IN_SUCCESS") {
-        return { userInfo: action.payload };
-    }
-
-    else {
-        return state;
-    }
-};
